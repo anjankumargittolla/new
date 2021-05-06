f695aa3c67eb18afe3bf977227a397d36bcecf13
anjankumargittolla <anjan111anju@gmail.com>
2021-05-06 11:21:29 +0530

    commit

---
+++ app/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+
+
+class Recipes(models.Model):
+    name = models.CharField(max_length=25)
+    ingredients = models.TextField(max_length=200)
+    process = models.TextField()
+    image = models.ImageField(upload_to="pictures/", default=timezone.now())
+
+
+    def __str__(self):
+        return self.name
